550b0bfd3edc3e5c99af987cd48ceb4b8028a71d
lilei <869859426@qq.com>
2019-03-15 16:34:27 +0800

    添加认证IdentityServer4,实现对Ocelot的api的访问

--- course1/Lil.AuthPlatform.Gateway/Startup.cs
+++ course1/Lil.AuthPlatform.Gateway/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
 using Lil.AuthPlatform.AhphOcelot.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Ocelot.Administration;
 using Ocelot.DependencyInjection;
 
 namespace Lil.AuthPlatform.Gateway
@@ -28,15 +30,21 @@ namespace Lil.AuthPlatform.Gateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            Action<IdentityServerAuthenticationOptions> option = o =>
+            {
+                o.Authority = "http://localhost:6611"; //IdentityServer地址
+                o.RequireHttpsMetadata = false;
+                o.ApiName = "gateway_admin"; //网关管理的名称，对应的为客户端授权的scope
+            };
             services.AddOcelot().AddAhphOcelot(options =>
             {
                 //options.DbConnectionStrings = "Data Source=LJY-DUDU\\MSSQLSERVER01;Integrated Security=SSPI;Initial Catalog=AphpOcelot";
                 options.DbConnectionStrings = "Data Source=LIL-AS-P2;Integrated Security=SSPI;Initial Catalog=AphpOcelot";
                 // 
-                options.EnableTimer = true;
-                options.TimerDelay = 1000 * 10;
-            });
+                //options.EnableTimer = true;
+                //options.TimerDelay = 1000 * 10;
+            })
+                .AddAdministration("/CtrOcelot",option);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
